77e239d4d3ceb507b00f118c9107366f1de52125
VirPalus <mario_moerman@icloud.com>
2026-03-10 12:23:04 +0100

    opkuis namen

--- JS/dotnet.js
+++ JS/dotnet.js
@@ -2,32 +2,32 @@ const oefeningen = [
   {
     type: "Sequentie",
     beschrijving: "Schrijf een programma dat een getal inleest.\nDaarna wordt het kwadraat afgedrukt van dit getal.\nDruk ook het kwadraat van de buren van dit getal af.",
-    code: "TXT/DotNet/Sequentie.txt",
+    code: "TXT/DotNet/sequentie.txt",
   },
   {
     type: "Selectie",
     beschrijving: "Schrijf een programma dat voor een bepaald coördinaat bepaalt in welk kwadrant dit ligt.\nDe gebruiker geeft een X-coördinaat in.\nDe gebruiker geeft een Y-coördinaat in.\nHet programma bepaalt vervolgens het juiste kwadrant.",
-    code: "TXT/DotNet/Selectie.txt",
+    code: "TXT/DotNet/selectie.txt",
   },
   {
     type: "For Loop",
     beschrijving: "Lees 2 symbolen in.\nVraag daarna een lengte.\nVraag daarna een breedte.\nDruk vervolgens een sjaal af met de ingevoerde symbolen, lengte en breedte.",
-    code: "TXT/DotNet/ForLoop.txt",
+    code: "TXT/DotNet/forLoop.txt",
   },
   {
     type: "Do While",
     beschrijving: "Lees voor 3 pijlen de landingsplaats.\nGeef vervolgens het totaal van de punten weer.\nIngevoerde waardes moeten gehele getallen zijn.\nDe waarden moeten groter zijn dan 0 en kleiner of gelijk aan 4.\nNa een correcte invoer wordt een nieuw scherm getoond.\n\nBuiten de roos: 0 punten (landingsplaats 1)\nBinnenste ring: 20 punten (landingsplaats 2)\nBuitenste ring: 50 punten (landingsplaats 3)\nIn het hart: 100 punten (landingsplaats 4)",
-    code: "TXT/DotNet/DoWhile.txt",
+    code: "TXT/DotNet/doWhile.txt",
   },
   {
     type: "While Do",
     beschrijving: "Vraag de gebruiker om een tafel in te geven.\nDe gebruiker kan dit blijven doen tot het getal 0 wordt ingegeven.\nWe tonen van elk ingegeven getal de tafel van 10.",
-    code: "TXT/DotNet/WhileDo.txt",
+    code: "TXT/DotNet/whileDo.txt",
   },
   {
     type: "Collecties",
     beschrijving: "Schrijf een programma dat de gebruiker vraagt om woorden in te geven.\nDeze woorden steek je in een lijst.\n\nPrint daarna het kortste woord.\nPrint ook het langste woord.\n\nPrint vervolgens de som af van de posities van het kortste en het langste woord in de lijst.",
-    code: "TXT/DotNet/Collecties.txt",
+    code: "TXT/DotNet/collecties.txt",
   },
 ];
 
